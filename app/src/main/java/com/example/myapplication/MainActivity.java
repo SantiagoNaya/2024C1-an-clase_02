4bfd1dada21c86e18ea87318c7d370dd48a53ec5
Santiago <santiagonaya190@gmail.com>
2024-04-22 11:59:20 -0300

    feat: agregue botones a la calculadora

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,55 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.R.id.displayText;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import calculator.Calculator;
 
 public class MainActivity extends AppCompatActivity {
+public void sumar(View view){
+    Log.i("app","funciona");
+    TextView displayText =  findViewById(R.id.displayText);
+    displayText.setText(R.string.Sumar);
+
+}
+
+    public void restar(View view){
+        Log.i("app","funciona");
+        TextView displayText =  findViewById(R.id.displayText);
+        displayText.setText(R.string.restar);
+
+    }
+    public void multiplicar(View view){
+        Log.i("app","funciona");
+        TextView displayText =  findViewById(R.id.displayText);
+        displayText.setText(R.string.multiplicar);
+
+    }
+    public void dividir(View view){
+        Log.i("app","funciona");
+        TextView displayText =  findViewById(R.id.displayText);
+        displayText.setText(R.string.dividir);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+         setContentView(R.layout.activity_main);
+          TextView displayText =  findViewById(R.id.displayText);
+           displayText.setText("probando pantalla");
         Calculator calculator = new Calculator();
         calculator.sumar(1,2);
         calculator.dividision(100,51);
 
      calculator.restar(10,5);
+
 
         }
     }
